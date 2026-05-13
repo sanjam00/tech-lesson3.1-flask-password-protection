62d2fd488ce292d820ec3b7ab92176ef1fb002aa
Sanaya James <sanaya.j.james@gmail.com>
2026-05-12 22:12:35 -0700

    completed lesson

--- server/models.py
+++ server/models.py
@@ -1,13 +1,34 @@
 from marshmallow import Schema, fields
-
-from config import db
+from sqlalchemy.ext.hybrid import hybrid_property
+from config import db, bcrypt
 
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
+    # password hash
+    _password_hash = db.Column(db.String)
 
+    
+    # method to protect password_hash property
+    @hybrid_property
+    def password_hash(self):
+        raise Exception('Passwod hashes may not be viewed.')
+    
+    # method to set password hash property using bcrypt.generate_password_hash()
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    # authenticate method that uses bcrypt.check_password_hash()
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
+    
     def __repr__(self):
         return f'User {self.username}, ID: {self.id}'
 
